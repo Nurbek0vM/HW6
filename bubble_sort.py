a93fff1517797098554f402c3182d66f90aafba1
NurbekOvM <Nurbekov.mairambek5@mail.ru>
2023-11-23 22:59:49 +0600

    New commit

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,31 +6,6 @@ def bubble_sort(lst):
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
     return lst
 
-
-def binary_search(lst, x):
-    low = 0
-    high = len(lst) - 1
-    mid = 0
-
-    while low <= high:
-        mid = (high + low) // 2
-        if lst[mid] < x:
-            low = mid + 1
-        elif lst[mid] > x:
-            high =  mid - 1
-        else:
-            return mid
-    return -1
-
-lst = [64, 34, 25, 12, 22, 11, 90]
-x = 22
-
-sorted_lst = bubble_sort(lst)
-result = binary_search(sorted_lst, x)
-
-print(f"Отсортированный список: {sorted_lst}")
-print(f"Результат двоичного поиска: {result}")
-
 def binary_search(element, lst):
     low = 0
     high = len(lst) - 1
